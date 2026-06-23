6974bc58fe8c344a351d94c949a233680dc7f6d4
SyFord421 <jaki05118@gmail.com>
2026-06-23 10:20:55 +0700

    feat: Creating UI design (In progress)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,44 @@
+use std::io;
 pub mod worker;
 pub mod model;
 pub mod ui;
 use model::{SystemStatus, Cpu, Ram, Disk, Battery, BatteryStatus, Network};
-
-
+use ratatui::{backend::CrosstermBackend, Terminal};
 use crossterm:: {
-    event::{self, DisableMouseCapture, EnableMouseCapture, event, KeyCode},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},    
+};
+
+
+
+fn main() -> Result<(), io::Error> {
+    let now_stat = SystemStatus::read_system();
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?; 
+
+    loop {
+        terminal.draw(|f| {
+            ui::draw_ui(f, &now_stat);
+
+        })?;
+        if event::poll(std::time::Duration::from_millis(100))? {
+            if let Event::Key(key) = event::read()?{
+                if key.code == KeyCode::Char('q'){
+                    break;
+                }
+            }
+        }   
+    } 
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(),
+             LeaveAlternateScreen,
+             DisableMouseCapture
+            )?;
+    terminal.show_cursor()?;
+    Ok(())
 }
-
-
-fn main() -> Result {
     
--- src/ui.rs
+++ src/ui.rs
@@ -1,43 +1,56 @@
 use ratatui::{
-    backend::Backend,
     layout::{Constraint, Direction, Layout},
     widgets::{Block, Borders, Paragraph},
     Frame,
 };
 use crate::model::SystemStatus;
 
-pub fn draw_ui<B: Backend>(f: &mut Frame<B>, status: &SystemStatus) {
+pub fn draw_ui(f: &mut Frame, status: &SystemStatus) {
     // bagi layar jadi 4 bagian Horizontal
-    let chunks = Layout::default()
-        .direction(Direction::Horizontal)
+    let ver_chunks = Layout::default()
+        .direction(Direction::Vertical)
         .constraints([
-            Constraint::Percentage(25), // CPU
-            Constraint::Percentage(25), // RAM
-            Constraint::Percentage(25), // DISK
-            Constraint::Percentage(25), // Battery
+            Constraint::Percentage(20), // CPU
+            Constraint::Percentage(20), // RAM
+            Constraint::Percentage(20), // DISK
+        ])
+        .split(f.size());
+
+    let hor_chuks = Layout::default() 
+        .direction(Direction::Horizontal) 
+        .constraints([
+            Constraint::Percentage(20), // Battery
+            Constraint::Percentage(20), // Network        
         ])
         .split(f.size());
 
     // widget buat CPU
     let cpu_box = Paragraph::new(format!(
-        "cpu usage: {}% load average: {} cpu temp: {}", 
-        status.cpu_status.cpu_usage, status.cpu_status.cpu_average, status.cpu_status.cpu_temp
+        "cpu usage: {}%\nload average: {}\ncpu temp: {}",
+        status.cpu_status.cpu_usage, status.cpu_status.load_average, status.cpu_status.cpu_temp
     ))
     .block(Block::default().title(" cpu stat ").borders(Borders::ALL));
-    f.render_widget(cpu_box, chunks[0]);
+    f.render_widget(cpu_box, ver_chunks[0]);
     
     // widget buat RAM
-    let ram_box = Paragraph::new(format!("ram usage: {}%", status.ram_status.ram_usage))
+    let ram_box = Paragraph::new(format!("Ram capacity: {}GB", status.ram_status.ram_capacity_gb))
         .block(Block::default().title(" ram stat ").borders(Borders::ALL));
-    f.render_widget(ram_box, chunks[1]);
+    f.render_widget(ram_box, ver_chunks[1]);
     
     // widget buat DISK
-    let disk_box = Paragraph::new(format!("disk usage: {}%", status.disk_status.disk_usage))
+    let disk_box = Paragraph::new(format!("disk usage: {}%", status.disk_status.disk_capacity_gb))
         .block(Block::default().title(" disk stat ").borders(Borders::ALL));
-    f.render_widget(disk_box, chunks[2]);
+    f.render_widget(disk_box, hor_chuks[0]);
             
-    // widget buat Battery (pake data battery_status juga yank)
-    let battery_box = Paragraph::new(format!("battery usage: {}%", status.battery_status.battery_usage))
+    // widget buat Battery
+    let battery_box = Paragraph::new(format!("battery usage: {}%", status.battery_status.health_percentage))
         .block(Block::default().title(" battery stat ").borders(Borders::ALL));
-    f.render_widget(battery_box, chunks[3]);
+    f.render_widget(battery_box, hor_chuks[1]);
+
+    // widget buat Network
+    let battery_box = Paragraph::new(format!("download speed {}KB/s", status.network_status.download_speed_kbps))
+        .block(Block::default().title(" battery stat ").borders(Borders::ALL));
+    f.render_widget(battery_box, ver_chunks[2C]);
+
+
 }
